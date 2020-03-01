d2d272b16f236606463d013c939b6232096482d0
iulianpro <iulian.prodea@gmail.com>
2020-03-01 11:31:07 +0000

    Added javascript send email

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "first_test_template", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            alert("Your message was successfully sent!");
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
