80a75127f8de9afa3da29653c2dc7f70e4bbd76d
iulianpro <iulian.prodea@gmail.com>
2020-10-01 10:58:59 +0100

    Added display phone and email on click

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -14,9 +14,23 @@ function sendMail(contactForm) {
             }
         );
     document.getElementById('submit-button').innerHTML = '<img class="spiner" src="assets/images/loading.gif" alt="">';
+
     setTimeout(function () {
         document.getElementById("myForm").reset();
         document.getElementById('submit-button').innerHTML = 'Send';
     }, 3000);
+    
     return false;  // To block from loading a new page
+}
+
+function showPhone() {
+    document.getElementById('personal-phone').innerHTML = '+44 7424 693338';
+    document.getElementById('personal-phone').classList.remove('personal-phone-hide');
+    document.getElementById('personal-phone').classList.add('personal-info-item');
+}
+
+function showEmail() {
+    document.getElementById('personal-email').innerHTML = 'iulian.prodea@outlook.com';
+    document.getElementById('personal-email').classList.remove('personal-email-hide');
+    document.getElementById('personal-email').classList.add('personal-info-item');
 }
