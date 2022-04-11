8693943498ee462f3ab313b80eb58602bfd14d71
iulianpro <iulian.prodea@gmail.com>
2022-04-11 21:52:59 +0100

    error fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     $('.project-item').mouseenter(function () {
         $(this).css('color', '#fff')
+        $(this).find('h4 a').addClass('text-white')
     });
     $('.project-item').mouseleave(function () {
         $(this).css('color', 'transparent')
+        $(this).find('h4 a').removeClass('text-white')
     });
 });
